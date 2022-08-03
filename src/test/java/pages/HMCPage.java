f89a2967bd6f3f127a4b7e0813d766dc1489bd9d
Meryem Aydin <meryemaydinsener@gmail.com>
2022-08-03 09:24:02 +0200

    baslangix

---
+++ src/test/java/pages/HMCPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+
+public class HMCPage {
+    //public HMCPage(){
+    //        PageFactory.initElements(Driver.getDriver(), this);
+    //    }
+    //    @FindBy(xpath = "(//a[@class='nav-link'])[7]")
+    //    public WebElement loginButonu;
+    //    @FindBy(xpath = "//input[@class='form-control required']")
+    //    public WebElement usernameKutusu;
+    //    @FindBy(xpath = "//input[@class='form-control required password']")
+    //    public WebElement passwordKutusu;
+    //    @FindBy(id = "btnSubmit")
+    //    public WebElement loginButonu2;
+    //    @FindBy(id = "menuHotels")
+    //    public WebElement hotelManagement;
+    //    @FindBy (xpath="//div[@class='validation-summary-errors']")
+    //    public WebElement yanlisGiris;
+    //    @FindBy(xpath = "(//i[@class='icon-calendar'])[2]")
+    //    public WebElement hotelList;
+    //    @FindBy(xpath = "//span[@class='hidden-480']")
+    //    public WebElement addHotel;
+    //    @FindBy(xpath = "//input[@name='Code']")
+    //    public WebElement codeKutusu;
+    //    @FindBy(xpath = "//button[@id='btnSubmit']")
+    //    public WebElement saveButonu;
+    //    @FindBy(xpath = "//select[@class='form-control input-lg required']")
+    //    public   WebElement ddm;
+}
